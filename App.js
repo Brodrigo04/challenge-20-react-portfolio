06dc053f0ec98270d41bf2ea99208d2c9e60759d
Brodrigo04 <knightarmy350@gmail.com>
2024-04-07 21:07:38 -0400

    added notfound.js and app.js

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
+
+
 // App.js
 import React from 'react';
 import './App.css';
-import Header from ' ./components/Header';
-import AboutMe from ' ./components/AboutMe';
-import Projects from ' ./components/Projects';
-import Contact from ' ./components/Contact';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import AboutMe from './components/AboutMe';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <main>
-        <AboutMe />
-        <Projects />
-        <Contact />
-      </main>
+      <Router>
+        <Header />
+        <main>
+          <Switch>
+            <Route exact path="/" component={AboutMe} />
+            <Route path="/projects" component={Projects} />
+            <Route path="/contact" component={Contact} />
+            <Route component={NotFound} />
+          </Switch>
+        </main>
+      </Router>
     </div>
   );
 }
